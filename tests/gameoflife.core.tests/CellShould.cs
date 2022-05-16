9e964bcec2a32410b48184b367342b4a7dc86146
Jacobus Meintjes <jacobus.meintjes@gmail.com>
2022-05-16 21:39:22 +0200

    Initialize Rule 1: Cell should die from under population

--- tests/gameoflife.core.tests/CellShould.cs
+++ tests/gameoflife.core.tests/CellShould.cs
@@ -6,12 +6,22 @@ namespace gameoflife.core.tests;
 public class CellShould
 {
     [Fact]
-    public void HaveTheStateItWasInitializedWith()
+    public void BeInTheStateItWasInitializedWith()
     {
         var aliveCell = new Cell(CellState.Alive);
         aliveCell.State.Should().Be(CellState.Alive);
         
         var deadCell = new Cell(CellState.Dead);
         deadCell.State.Should().Be(CellState.Dead);        
+    }
+
+    [Fact]
+    public void DieFromUnderPopulationIfTheCellHasFewerThanTwoLiveNeighbours()
+    {
+        var aliveCellWithZeroNeighbours = new Cell(CellState.Alive);
+        aliveCellWithZeroNeighbours.NextState(0).Should().Be(CellState.Dead);
+        
+        var aliveCellWithOneNeighbour = new Cell(CellState.Alive);
+        aliveCellWithOneNeighbour.NextState(1).Should().Be(CellState.Dead);
     }
 }
