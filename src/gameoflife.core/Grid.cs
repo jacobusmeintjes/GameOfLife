e1991923d15c6fb2638f39cfdfefeb8117cb6b66
Jacobus Meintjes <jacobus.meintjes@gmail.com>
2022-05-16 22:02:22 +0200

    Add next scenario and test cells are alive

--- src/gameoflife.core/Grid.cs
+++ src/gameoflife.core/Grid.cs
@@ -4,9 +4,12 @@ public class Grid
 {
     public int Rows { get;  }
     public int Columns { get; }
+    public Cell[,] Cells { get; }
     public Grid(int rows, int columns)
     {
         Rows = rows;
         Columns = columns;
+
+        Cells = new Cell[Rows, Columns];
     }
 }
--- tests/gameoflife.core.tests/GridShould.cs
+++ tests/gameoflife.core.tests/GridShould.cs
@@ -13,5 +13,21 @@ public class GridShould
 
         grid.Rows.Should().Be(4);
         grid.Columns.Should().Be(8);
+
+        grid.Cells.Length.Should().Be(32);
+    }
+    
+    [Fact]
+    public void BeInitializedWithRowsAndColumnsAndRandomizeCellState()
+    {
+        var grid = new Grid(4, 8);
+
+        grid.Rows.Should().Be(4);
+        grid.Columns.Should().Be(8);
+
+        foreach (var cell in grid.Cells)
+        {
+            cell.Should().NotBeNull();
+        }
     }
 }
