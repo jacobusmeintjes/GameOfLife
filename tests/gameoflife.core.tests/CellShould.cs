9e17da16c49344d5c394a04c3c7bd9b9101d9ece
Jacobus Meintjes <jacobus.meintjes@gmail.com>
2022-05-16 21:45:25 +0200

    Add next scenario with failing logic

--- tests/gameoflife.core.tests/CellShould.cs
+++ tests/gameoflife.core.tests/CellShould.cs
@@ -10,9 +10,9 @@ public class CellShould
     {
         var aliveCell = new Cell(CellState.Alive);
         aliveCell.State.Should().Be(CellState.Alive);
-        
+
         var deadCell = new Cell(CellState.Dead);
-        deadCell.State.Should().Be(CellState.Dead);        
+        deadCell.State.Should().Be(CellState.Dead);
     }
 
     [Fact]
@@ -20,16 +20,26 @@ public class CellShould
     {
         var aliveCellWithZeroNeighbours = new Cell(CellState.Alive);
         aliveCellWithZeroNeighbours.NextState(0).Should().Be(CellState.Dead);
-        
+
         var aliveCellWithOneNeighbour = new Cell(CellState.Alive);
         aliveCellWithOneNeighbour.NextState(1).Should().Be(CellState.Dead);
     }
-    
-    
+
+
     [Fact]
     public void DieFromOvercrowdingIfMoreThanThreeLiveNeighbours()
     {
         var aliveCellWithThreeNeighbours = new Cell(CellState.Alive);
         aliveCellWithThreeNeighbours.NextState(4).Should().Be(CellState.Dead);
+    }
+
+    [Fact]
+    public void LiveOnIfTwoOrThreeLiveNeighbours()
+    {
+        var aliveCellWithTwoNeighbours = new Cell(CellState.Alive);
+        aliveCellWithTwoNeighbours.NextState(2).Should().Be(CellState.Alive);
+
+        var aliveCellWithThreeNeighbours = new Cell(CellState.Alive);
+        aliveCellWithThreeNeighbours.NextState(3).Should().Be(CellState.Alive);
     }
 }
