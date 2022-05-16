71529f3bde85a79d05c7695a45fc67a73f573622
Jacobus Meintjes <jacobus.meintjes@gmail.com>
2022-05-16 21:53:16 +0200

    Add Grid type

---
+++ tests/gameoflife.core.tests/GridShould.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+namespace gameoflife.core.tests;
+
+public class GridShould
+{
+    [Fact]
+    public void BeInitializedWithRowsAndColumns()
+    {
+        var grid = new Grid(4, 8);
+    }
+}
